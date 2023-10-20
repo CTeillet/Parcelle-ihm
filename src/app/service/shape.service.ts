24862db93a971a8d85f7865ec1520703f3430f48
Teillet Corentin <teillet.corentin@neuf.fr>
2023-10-20 19:15:35 +0200

    modification port

--- src/app/service/shape.service.ts
+++ src/app/service/shape.service.ts
@@ -8,21 +8,21 @@ export class ShapeService {
   constructor(private http: HttpClient) { }
 
   getParcellesShapes() {
-    return this.http.get('http://localhost:8081/parcelle');
+    return this.http.get('http://localhost:8080/parcelle');
     // return this.http.get('assets/data/parcelles.json');
     // return this.http.get('assets/data/parcelle1.json');
   }
 
   //send a delete request to the API parcelle with the list of id in the body we want to delete. The ids are string, and we send a delete request
   deleteParcelles(ids: string[]) {
-    return this.http.delete('http://localhost:8081/parcelle', {
+    return this.http.delete('http://localhost:8080/parcelle', {
       body: ids
     });
   }
 
   //send a get request to the API generatePateTemporaires
   generatePateTemporaires() {
-    return this.http.get('http://localhost:8081/generatePateTemporaires');
+    return this.http.get('http://localhost:8080/generatePateTemporaires');
     // return this.http.get('assets/data/pates.json');
   }
 
