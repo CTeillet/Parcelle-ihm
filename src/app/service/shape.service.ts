a86832a82024e101ef040f72beb9bb854d4f914c
Teillet Corentin <teillet.corentin@neuf.fr>
2023-10-21 14:49:39 +0200

    mise en place d'environnement

--- src/app/service/shape.service.ts
+++ src/app/service/shape.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
@@ -8,21 +9,19 @@ export class ShapeService {
   constructor(private http: HttpClient) {}
 
   getParcellesShapes() {
-    return this.http.get('http://localhost:8080/parcelle');
-    // return this.http.get('assets/data/parcelles.json');
-    // return this.http.get('assets/data/parcelle1.json');
+    return this.http.get(environment.apiUrl + '/parcelle');
   }
 
   //send a delete request to the API parcelle with the list of id in the body we want to delete. The ids are string, and we send a delete request
   deleteParcelles(ids: string[]) {
-    return this.http.delete('http://localhost:8080/parcelle', {
+    return this.http.delete(environment.apiUrl + '/parcelle', {
       body: ids,
     });
   }
 
   //send a get request to the API generatePateTemporaires
   generatePateTemporaires() {
-    return this.http.get('http://localhost:8080/generatePateTemporaires');
+    return this.http.get(environment.apiUrl + '/generatePateTemporaires');
     // return this.http.get('assets/data/pates.json');
   }
 }
